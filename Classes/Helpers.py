5473e7d4b9e07a33df402fa72e5459e2416cde61
Mohammad Metwally <rashaali84@gmail.com>
2021-12-04 20:08:18 +0100

    Classes and helpers creation
    This push has classes and some supporting functions

---
+++ Classes/Helpers.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+# these are the enumeration types needed
+# rail pass type enum
+class RailPassType(Enum):
+    SINGLE = 1
+    MULTI = 2
+
+
+# rail pass level enum
+class RailPassLevel(Enum):
+    VIP = 1
+    ECONOMIC = 2
+
+
+# methods to help the conversions from user inputs to enums and vice versa
+def from_user_input_type(input_value):
+    if input_value == 1:
+        return RailPassType.SINGLE
+    elif input_value == 2:
+        return RailPassType.MULTI
+    else:
+        raise NotImplementedError
+
+
+def from_user_input_level(input_value):
+    if input_value == 1:
+        return RailPassLevel.VIP
+    elif input_value == 2:
+        return RailPassLevel.ECONOMIC
+    else:
+        raise NotImplementedError
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from Classes.RailPass import RailPass
+from Classes.person import Person
+
+
 def main():
-    pass
+    #testing
+    person = Person('name','description',0,'lastName','first_name','email_address@gmail.com','password',12345678,12345678)
+    rail = RailPass('name','description',10,1,2,5,1,0 ,False ,'2021-07-04 18:38:09.710949' , '2021-12-04 18:38:09.710949')
+
+    person.buy_ticket(rail)
+    print(person)
 
 if __name__ == '__main__':
     main()
