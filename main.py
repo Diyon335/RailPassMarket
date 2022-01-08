c573f872b3e46a43f4bbae51045be12485c8627f
Rasha Ali <rashaali84@gmail.com>
2022-01-08 11:27:44 +0100

    changes in the import line of main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ authors: Dirk Peeters, Rasha Ali, George Paul, Diyon Wickrameratne
 """
 from Classes.person import Person
 from rail_pass_system import RailPassSystem
-from Classes.Helpers import invalid_string, valid_email
+from Classes.helpers import invalid_string, valid_email
 
 """
 Main method that runs the program
